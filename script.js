3a8161c3edf5ce6e87d2e81c38a7b1104174c077
Prak005 <prakharmallick005@gmail.com>
2025-07-20 11:57:13 +0530

    Edit function playRound

--- script.js
+++ script.js
@@ -18,4 +18,30 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
     humanChoice = humanChoice.toLowerCase();
+    if(humanChoice === computerChoice)
+        console.log("Tie! Draw Again");
+    else if(humanChoice==="rock" && computerChoice==="scissors"){
+        console.log("You Win! Rock beats Scissors");
+        humanScore++;
+    }
+    else if(humanChoice==="rock" && computerChoice==="paper"){
+        console.log("You Lose! Paper beats Rock");
+        computerScore++;
+    }
+    else if(humanChoice==="scissors" && computerChoice==="rock"){
+        console.log("You Lose! Rock beats Scissors");
+        computerScore++;
+    }
+    else if(humanChoice==="scissors" && computerChoice==="paper"){
+        console.log("You Win! Scissors beats Paper");
+        humanScore++;
+    }
+    else if(humanChoice==="paper" && computerChoice==="rock"){
+        console.log("You Win! Paper beats Rock");
+        humanScore++;
+    }
+    else if(humanChoice==="paper" && computerChoice==="scissors"){
+        console.log("You Lose! Scissors beats Paper");
+        computerScore++;
+    }
 }
